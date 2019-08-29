6caa80b09731c96bb4a11733ed28bfbe796540c2
Tom Monks <monksy@gmail.com>
2019-08-29 15:06:42 -0400

    Initial draft with Reactstrap, Navbar, and input & output areas

--- src/App.js
+++ src/App.js
@@ -1,26 +1,67 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import {
+  Container,
+  Row,
+  Col,
+  Navbar,
+  NavbarBrand,
+  Form,
+  FormGroup,
+  Label,
+  Input
+} from "reactstrap";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      markdown: "Enter Markdown here"
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({ markdown: event.target.value });
+  }
+
+  render() {
+    return (
+      <Container fluid>
+        <Navbar color="light" light>
+          <NavbarBrand>Markdown Previewer</NavbarBrand>
+        </Navbar>
+        <Row>
+          <Col sm="6">
+            <Form>
+              <FormGroup>
+                <Label for="input">Markdown Input</Label>
+                <Input
+                  type="textarea"
+                  name="input"
+                  id="input"
+                  value={this.state.markdown}
+                  onChange={this.handleChange}
+                ></Input>
+              </FormGroup>
+            </Form>
+          </Col>
+          <Col sm="6">
+            <p>HTML Output</p>
+            <div
+              id="output"
+              style={{ border: "1px solid lightgray", borderRadius: 5 }}
+            >
+              {this.state.markdown}
+            </div>
+          </Col>
+        </Row>
+      </Container>
+    );
+  }
 }
 
 export default App;
