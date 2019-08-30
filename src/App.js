840f8a14b8193c960e758c3c4f77221c81c143ae
Tom Monks <monksy@gmail.com>
2019-08-30 14:29:00 -0400

    Added react-fcctest, marked, and DOMPurifier Got all FCC tests passing!

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React from "react";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import marked from "marked";
+import ReactFCCTest from "react-fcctest";
+import DOMPurify from "dompurify";
 import {
   Container,
   Row,
@@ -13,12 +16,43 @@ import {
   Input
 } from "reactstrap";
 
+marked.setOptions({
+  gfm: true,
+  breaks: true
+});
+
 class App extends React.Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      markdown: "Enter Markdown here"
+      markdown: `# Heading 1
+[freeCodeCamp](https://freecodecamp.org)
+
+## Heading 2
+Some \`inline code\`
+
+\`\`\`
+echo 'Hello World'
+\`\`\` 
+
+### Heading 3
+
+Here is how you do a list:
+- Item 1
+- Item 2
+- Item 3
+
+### Block Quotes
+
+> Here is how you do a block quote
+> Block quote line 2
+
+**This is how you bold text**
+
+### Images
+![freeCodeCamp Logo](https://d33wubrfki0l68.cloudfront.net/e66392bc2941ad564b6956321e778f29073cf585/4190b/img/glyph-alternative.svg "freeCodeCamp Logo")
+`
     };
 
     this.handleChange = this.handleChange.bind(this);
@@ -26,11 +60,19 @@ class App extends React.Component {
 
   handleChange(event) {
     this.setState({ markdown: event.target.value });
+    document.getElementById("preview").innerHTML = marked(
+      DOMPurify.sanitize(event.target.value)
+    );
+  }
+
+  componentDidMount() {
+    document.getElementById("preview").innerHTML = marked(this.state.markdown);
   }
 
   render() {
     return (
       <Container fluid>
+        <ReactFCCTest />
         <Navbar color="light" light>
           <NavbarBrand>Markdown Previewer</NavbarBrand>
         </Navbar>
@@ -38,11 +80,11 @@ class App extends React.Component {
           <Col sm="6">
             <Form>
               <FormGroup>
-                <Label for="input">Markdown Input</Label>
+                <Label for="editor">Markdown Input</Label>
                 <Input
                   type="textarea"
-                  name="input"
-                  id="input"
+                  name="editor"
+                  id="editor"
                   value={this.state.markdown}
                   onChange={this.handleChange}
                 ></Input>
@@ -50,12 +92,15 @@ class App extends React.Component {
             </Form>
           </Col>
           <Col sm="6">
-            <p>HTML Output</p>
-            <div
-              id="output"
-              style={{ border: "1px solid lightgray", borderRadius: 5 }}
-            >
-              {this.state.markdown}
+            <p>Formatted Output</p>
+            <div id="preview"></div>
+          </Col>
+        </Row>
+        <Row>
+          <Col sm="12">
+            <p>Purified output for testing</p>
+            <div style={{ border: "1px solid lightgray" }}>
+              {DOMPurify.sanitize(this.state.markdown)}
             </div>
           </Col>
         </Row>
