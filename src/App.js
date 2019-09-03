915c0b29bc46ca8e7bbefe6c40d18a6a322c14a7
Tom Monks <monksy@gmail.com>
2019-09-03 15:27:35 -0400

    Finalized full-heigh layout Got dark navbar working Centered the navbar-brand

--- src/App.js
+++ src/App.js
@@ -1,17 +1,10 @@
 import React from "react";
 import "./App.css";
-import "bootstrap/dist/css/bootstrap.min.css";
 import marked from "marked";
 import ReactFCCTest from "react-fcctest";
 import DOMPurify from "dompurify";
-import {
-  Container,
-  Row,
-  Col,
-  Navbar,
-  NavbarBrand,
-  Input
-} from "reactstrap";
+import { Container, Row, Col, Navbar, NavbarBrand, Input } from "reactstrap";
+import INITIAL_MARKDOWN from "./markdown";
 
 marked.setOptions({
   gfm: true,
@@ -23,33 +16,7 @@ class App extends React.Component {
     super(props);
 
     this.state = {
-      markdown: `# Heading 1
-[freeCodeCamp](https://freecodecamp.org)
-
-## Heading 2
-Some \`inline code\`
-
-\`\`\`
-echo 'Hello World'
-\`\`\` 
-
-### Heading 3
-
-Here is how you do a list:
-- Item 1
-- Item 2
-- Item 3
-
-### Block Quotes
-
-> Here is how you do a block quote
-> Block quote line 2
-
-**This is how you bold text**
-
-### Images
-![freeCodeCamp Logo](https://d33wubrfki0l68.cloudfront.net/e66392bc2941ad564b6956321e778f29073cf585/4190b/img/glyph-alternative.svg "freeCodeCamp Logo")
-`
+      markdown: INITIAL_MARKDOWN
     };
 
     this.handleChange = this.handleChange.bind(this);
@@ -60,15 +27,17 @@ Here is how you do a list:
   }
 
   createHTML() {
-    return {__html: marked(DOMPurify.sanitize(this.state.markdown))}
+    return { __html: marked(DOMPurify.sanitize(this.state.markdown)) };
   }
 
   render() {
     return (
       <Container fluid>
         <ReactFCCTest />
-        <Navbar light color="light">
-          <NavbarBrand>Markdown Previewer</NavbarBrand>
+        <Navbar dark color="dark">
+          <NavbarBrand href="#'" className="mx-auto">
+            Markdown Previewer
+          </NavbarBrand>
         </Navbar>
         <Row>
           <Col lg="6">
@@ -86,11 +55,14 @@ Here is how you do a list:
           <Col lg="6">
             <div className="pane">
               <div className="pane-title">Preview</div>
-              <div id="preview" dangerouslySetInnerHTML={this.createHTML()}></div>
+              <div
+                id="preview"
+                dangerouslySetInnerHTML={this.createHTML()}
+              ></div>
             </div>
           </Col>
         </Row>
-     </Container>
+      </Container>
     );
   }
 }
