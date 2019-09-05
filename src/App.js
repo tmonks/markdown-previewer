a423548ee55a267231ba3b2857a2a29e4532e6ce
Tom Monks <monksy@gmail.com>
2019-09-05 14:13:41 -0400

    Broke out into separate Header, EditorPane, and PreviewPane components. Added gh-pages

--- src/App.js
+++ src/App.js
@@ -2,9 +2,9 @@ import React from "react";
 import "./App.css";
 import marked from "marked";
 import ReactFCCTest from "react-fcctest";
-import DOMPurify from "dompurify";
+import DOMPurify from "dompurify"; // used to clean user input and prevent XSS attacks
 import { Container, Row, Col, Navbar, NavbarBrand, Input } from "reactstrap";
-import INITIAL_MARKDOWN from "./markdown";
+import INITIAL_MARKDOWN from "./markdown"; // sample markdown initially loaded in editor
 
 marked.setOptions({
   gfm: true,
@@ -26,43 +26,69 @@ class App extends React.Component {
     this.setState({ markdown: event.target.value });
   }
 
-  createHTML() {
-    return { __html: marked(DOMPurify.sanitize(this.state.markdown)) };
-  }
-
   render() {
     return (
       <Container fluid>
         <ReactFCCTest />
-        <Navbar dark color="dark">
-          <NavbarBrand href="#'" className="mx-auto">
-            Markdown Previewer
-          </NavbarBrand>
-        </Navbar>
+        <Header />
         <Row>
           <Col lg="6">
-            <div className="pane">
-              <div className="pane-title">Input</div>
-              <Input
-                type="textarea"
-                name="editor"
-                id="editor"
-                value={this.state.markdown}
-                onChange={this.handleChange}
-              ></Input>
-            </div>
+            <EditorPane
+              markdown={this.state.markdown}
+              handleChange={this.handleChange}
+            />
           </Col>
           <Col lg="6">
-            <div className="pane">
-              <div className="pane-title">Preview</div>
-              <div
-                id="preview"
-                dangerouslySetInnerHTML={this.createHTML()}
-              ></div>
-            </div>
+            <PreviewPane markdown={this.state.markdown} />
           </Col>
         </Row>
       </Container>
+    );
+  }
+}
+
+function Header() {
+  return (
+    <Navbar dark color="dark">
+      <NavbarBrand href="#'" className="mx-auto">
+        Markdown Previewer
+      </NavbarBrand>
+    </Navbar>
+  );
+}
+
+const EditorPane = props => {
+  return (
+    <div className="pane">
+      <div className="pane-title">Input</div>
+      <Input
+        type="textarea"
+        name="editor"
+        id="editor"
+        value={props.markdown}
+        onChange={props.handleChange}
+      ></Input>
+    </div>
+  );
+};
+
+class PreviewPane extends React.Component {
+  constructor(props) {
+    super(props);
+    this.createHTML = this.createHTML.bind(this);
+  }
+
+  // takes markdown and provides sanitized HTML for dangerouslySetInnerHTML
+  createHTML() {
+    return { __html: marked(DOMPurify.sanitize(this.props.markdown)) };
+  }
+
+  render() {
+    return (
+      <div className="pane">
+        <div className="pane-title">Preview</div>
+        <div id="preview" dangerouslySetInnerHTML={this.createHTML()}></div>
+      </div>
     );
   }
 }
